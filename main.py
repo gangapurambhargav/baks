d1b02cd65ce819f2724a09d0df3d2d61a28670ef
Gangapuram Bhargav <gangapurambhargav1@gmail.com>
2021-04-22 04:03:40 +0530

    u

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
 #Import libs
+import datetime
 import speech_recognition as sr 
 r =sr.Recognizer()
 with sr.Microphone() as source:
     #
     audio=r.listen(source)
 text = r.recognize_google(audio)
-print (text)
+
+if str(text).lower()== "What time is":
+    print(datetime.datetime.now().strftime('%b-%d-%I%M%p-%G'))
+    break
